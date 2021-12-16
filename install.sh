fdf3c1f4ab3300ea93f84176d190fd3cddb1b144
Aiden <aiden.storey@shopify.com>
2021-12-16 13:09:47 -0500

    Update install to add streamdeck

--- install.sh
+++ install.sh
@@ -13,4 +13,14 @@ touch ~/dotfiles/command/_local.sh
 if ! [ $SPIN ]; then
     mkdir -p ~/dotfiles/script
     mkdir -p ~/dotfiles/data
+
+    working_dir=$PWD
+
+    for plugin in ~/dotfiles/streamdeck/*; do
+        cd $plugin
+        ln -s $plugin ~/Library/Application\ Support/com.elgato.StreamDeck/Plugins/
+        $plugin/install.sh
+    done
+
+    cd $working_dir
 fi
